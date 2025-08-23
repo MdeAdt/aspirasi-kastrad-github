41087426e0aeffd472bee6b907969b5123c8320b
I Made Aditya Nugraha <madeadit435@gmail.com>
2025-08-23 20:36:38 +0700

    Restrukturisasai proyek untuk Verceel

--- api/index.js
+++ api/index.js
@@ -7,12 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Langsung connect ke MONGO_URI dari Environment Variables
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB terhubung...'))
   .catch(err => console.error('Gagal terhubung ke MongoDB:', err));
 
+// Path ke routes tidak perlu diubah karena mereka relatif terhadap file ini
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/aspirations', require('./routes/aspirations'));
 
-// PENTING: Untuk Vercel, kita export 'app'
+// Hapus baris app.listen dan ganti dengan module.exports
+// Vercel akan menangani servernya secara otomatis
 module.exports = app;
